4767c592f4d219078e3d5612b1cf461bc64dba97
abhishekmallik chevron.com <abhishekmallik@chevron.com>
2022-03-01 09:13:08 +0000

    make stable

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -31,30 +31,12 @@ module.exports = cds.service.impl(async (srv) => {
     const { MaintenanceOrderAnalytics } = srv.entities;
 
     srv.on(['READ'], MaintenanceOrderAnalytics, async (req) => {
-<<<<<<< HEAD
-
-        // let sMeasureParam = 'Counter';
-        // let filteredGroupBy = req.query.SELECT.groupBy ? req.query.SELECT.groupBy.filter(item => {
-        //     if (item.ref[sMeasureParam]) {
-        //         return false;
-        //     }
-        //     return true;
-        // }) : null;
-
-        // Report 2: Work Orders awaiting information outside Maintenance group
-        // let internalFilters = [
-        //     { ref: ['MaintOrdProcessSubPhaseCode'] }, '=', { val: '0035' }, 'and',
-        //     { ref: ['ConcatenatedOrderPhaseCtrlText'] }, '=', { val: 'H001' },
-        // ];
-=======
->>>>>>> 4963d3c92a146789c96efd2bb7c2af93df0daac0
 
         let query = SELECT.from(req.query.SELECT.from)
             .columns(req.query.SELECT.columns)
             .limit(req.query.SELECT.limit);
 
         if (req.query.SELECT.where) {
-<<<<<<< HEAD
 
             query.where(req.query.SELECT.where);
 
@@ -68,11 +50,6 @@ module.exports = cds.service.impl(async (srv) => {
             //         ...internalFilters,
             //     ]);
 
-=======
-            query.where([
-                ...req.query.SELECT.where
-            ]);
->>>>>>> 4963d3c92a146789c96efd2bb7c2af93df0daac0
         }
 
         if (req.query.SELECT.orderBy) {
@@ -80,26 +57,16 @@ module.exports = cds.service.impl(async (srv) => {
         }
 
         if (req.query.SELECT.groupBy) {
-<<<<<<< HEAD
             // Ignore GroupBy as it is not supported by API ODATA V2 in CSN.
             // query.groupBy(req.query.SELECT.groupBy)
         }
 
         // console.log(query)
-=======
-            // Ignore GroupBy as it is not supported by API ODATA V2.
-            // query.groupBy(req.query.SELECT.groupBy)
-        }
-
-        console.log(req.query);
->>>>>>> 4963d3c92a146789c96efd2bb7c2af93df0daac0
         let results = await API_MAINTENANCEORDER.tx(req).send({
             query: query,
         });
-        console.log(results);
-        return results;
+        // console.log(results);
 
-<<<<<<< HEAD
         let finalRes = await Promise.all(results);
         console.log(req.query.SELECT);
         if (req.query.SELECT.groupBy) {
@@ -133,38 +100,6 @@ module.exports = cds.service.impl(async (srv) => {
     // srv.on(['READ'], I_PMNotificationPriority, async (req) => {
     //     return API_MAINTENANCEORDER.tx(req).run(req.query);
     // });
-=======
-        // let finalRes = await Promise.all(results);
-        // return finalRes;
-
-        // if (req.query.SELECT.groupBy) {
-        //     let result = [];
-        //     let cond = req.query.SELECT.columns.map(col => {
-        //         return col.ref[0];
-        //     }).join();
-
-        //     result = alasql('SELECT COUNT(*) AS InternalCount,' + cond + ' FROM ? GROUP BY ' + cond + ' ORDER BY ' + cond, [finalRes]);
-        //     result = result.map(item => {
-        //         item.Counter = item.InternalCount;
-        //         delete item.InternalCount;
-        //         return item;
-        //     });
-
-        //     // console.log(result);
-
-        //     return result;
-        // }
-
-        // if (req.query.SELECT.count) {
-        //     finalRes['$count'] = finalRes.length
-        // }
-
-        // // console.log(results.length);
-
-        // return finalRes;
-
-    });
->>>>>>> 4963d3c92a146789c96efd2bb7c2af93df0daac0
 
 });
 
